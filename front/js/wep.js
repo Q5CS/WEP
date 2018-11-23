057edc6b476e014a7555d058695b03ab15ae21ae
OrdosX <i@ordosx.tech>
2018-11-23 14:16:31 +0800

    初始上传

---
+++ front/js/wep.js
@@ -0,0 +1,382 @@
+function initPage() {
+    if ($.cookie("name") == null) {
+        $("#name").text("请登录");
+        $("#user").removeAttr("data-toggle");
+        $("#user").attr("href", "/login")
+    } else {
+        $("#name").text($.cookie("name"))
+    }
+}
+
+function initDashboard() {
+    initPage()
+    $.base64.utf8decode = true;
+    var original = $.base64.atob($("#tData").text());
+    if (original == "Empty Set") {
+        $("#create").html("<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody><tr><td colspan=\"7\">这里什么都没有</td></tr></tbody>");
+        $("#match").html("<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody><tr><td colspan=\"7\">这里什么都没有</td></tr></tbody>");
+        return;
+    } else {
+        var all = original.split("||")
+        if (all[0] != "") {
+            var cTotal = all[0].split("/");
+            var cResult = "<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody>";
+            for (let i = 0; i < cTotal.length - 1; i++) {
+                const item = cTotal[i].split("|");
+                var res = "";
+                switch (item[5]) {
+                    case "0":
+                        res += `<tr class="info">`;
+                        break;
+                    case "1":
+                        res += `<tr class="warning">`;
+                        break;
+                    case "2":
+                        res += `<tr>`;
+                        break;
+                    case "3":
+                        res += `<tr>`;
+                        break;
+                }
+                res += "<td>" + item[0] + "</td>";
+                switch (item[1]) {
+                    case "0":
+                        res += "<td>" + "大作业本" + "</td>";
+                        break;
+                    case "1":
+                        res += "<td>" + "小作业本" + "</td>";
+                        break;
+                    case "2":
+                        res += "<td>" + "英语本" + "</td>";
+                        break;
+                    case "3":
+                        res += "<td>" + "语文作文本" + "</td>";
+                        break;
+                    case "4":
+                        res += "<td>" + "英语作文本" + "</td>";
+                        break;
+                }
+                res += "<td>" + item[2] + "</td>";
+                switch (item[3]) {
+                    case "0":
+                        res += "<td>" + "入手" + "</td>";
+                        break;
+                    case "1":
+                        res += "<td>" + "出手" + "</td>";
+                        break;
+                }
+                res += "<td>" + item[4] + "</td>";
+                switch (item[5]) {
+                    case "0":
+                        res += `<td>待匹配</td><td><button class="btn" type="button" onclick="del('` + item[0] + `')">删除</button></td>`
+                        break;
+                    case "1":
+                        res += `<td>待流通</td><td><button class="btn" type="button" onclick="showInfo('0', '` + item[0] + `')">查看对方信息</button>&nbsp;&nbsp;<button class="btn" type="button" onclick="reject('` + item[0] + `')">拒绝</button>&nbsp;&nbsp;<button class="btn" type="button" onclick="confirm('` + item[0] + `')">确认</button></td>`
+                        break;
+                    case "2":
+                        res += `<td>已完成</td><td><button class="btn" type="button" onclick="showInfo('0', '` + item[0] + `')">查看对方信息</button></td>`
+                        break;
+                    case "3":
+                        res += `<td>已关闭</td><td><button class="btn" type="button" onclick="showInfo('0', '` + item[0] + `')">查看对方信息</button></td>`
+                        break;
+                }
+                cResult += res + "</tr>";
+            }
+            $("#create").html(cResult + "</tbody>");
+        } else {
+            $("#create").html("<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody><tr><td colspan=\"7\">这里什么都没有</td></tr></tbody>");
+        }
+        if (all[1] != "") {
+            var mTotal = all[1].split("/");
+            var mResult = "<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody>";
+            for (let i = 0; i < mTotal.length - 1; i++) {
+                const item = mTotal[i].split("|");
+                var res = "";
+                switch (item[5]) {
+                    case "0":
+                        res += "<tr class=\"info\">";
+                        break;
+                    case "1":
+                        res += "<tr class=\"warning\">";
+                        break;
+                    case "2":
+                        res += "<tr>";
+                        break;
+                    case "3":
+                        res += "<tr>";
+                        break;
+                }
+                res += "<td>" + item[0] + "</td>";
+                switch (item[1]) {
+                    case "0":
+                        res += "<td>" + "大作业本" + "</td>";
+                        break;
+                    case "1":
+                        res += "<td>" + "小作业本" + "</td>";
+                        break;
+                    case "2":
+                        res += "<td>" + "英语本" + "</td>";
+                        break;
+                    case "3":
+                        res += "<td>" + "语文作文本" + "</td>";
+                        break;
+                    case "4":
+                        res += "<td>" + "英语作文本" + "</td>";
+                        break;
+                }
+                res += "<td>" + item[2] + "</td>";
+                switch (item[3]) {
+                    case "0":
+                        res += "<td>" + "入手" + "</td>";
+                        break;
+                    case "1":
+                        res += "<td>" + "出手" + "</td>";
+                        break;
+                }
+                res += "<td>" + item[4] + "</td>";
+                switch (item[5]) {
+                    case "0":
+                        res += `<td>待匹配</td><td><button class="btn" type="button" onclick="del('` + item[0] + `')">删除</button></td>`
+                        break;
+                    case "1":
+                        res += `<td>待流通</td><td><button class="btn" type="button" onclick="showInfo('1', '` + item[0] + `')">查看对方信息</button>&nbsp;&nbsp;<button class="btn" type="button" onclick="cancel('` + item[0] + `')">取消</button></td>`
+                        break;
+                    case "2":
+                        res += `<td>已完成</td><td><button class="btn" type="button" onclick="showInfo('1', '` + item[0] + `')">查看对方信息</button></td>`
+                        break;
+                    case "3":
+                        res += `<td>已关闭</td><td><button class="btn" type="button" onclick="showInfo('1', '` + item[0] + `')">查看对方信息</button></td>`
+                        break;
+                }
+                mResult += res + "</tr>";
+            }
+            $("#match").html(mResult + "</tbody>");
+        } else {
+            $("#match").html("<thead><tr><th>ID</th><th>物品</th><th>数量</th><th>类型</th><th>创建时间</th><th>状态</th><th>操作</th></tr></thead><tbody><tr><td colspan=\"7\">这里什么都没有</td></tr></tbody>");
+        }
+    }
+}
+
+function initMarketPlace() {
+    initPage()
+    $.base64.utf8decode = true;
+    var tables = new Array($("#BigBook tbody"), $("#SmallBook tbody"), $("#EnglishBook tbody"), $("#ChineseComp tbody"), $("#EnglishComp tbody"))
+    var original = $.base64.atob($("#tData").text())
+    var all = original.split("||")
+    for (let i = 0; i < all.length - 1; i++) {
+        if (all[i] == "-") {
+            tables[i].append(`<tr><td colspan="5" class="text-center">这里什么都没有</td></tr>`)
+        } else {
+            var items = all[i].split("/")
+            for (let j = 0; j < items.length - 1; j++) {
+                var item = items[j].split("|")
+                if (item[2] == "0") {
+                    item[2] = "入手"
+                } else {
+                    item[2] = "出手"
+                }
+                var data = `<tr><td>` + item[0] + `</td><td>` + item[1] + `</td><td>` + item[2] + `</td><td>` + item[3] + `</td><td>` + item[4] + `</td><td><button class="btn" type="button" onclick="match('` + item[0] + `')">匹配</button></td></tr>`
+                /* alert(data) */
+                tables[i].append(data)
+            }
+        }
+    }
+}
+
+function login() {
+    $.base64.utf8encode = true
+    var json = {
+        username: $("#username").val(),
+        password: $.base64.encode($("#password").val())
+    };
+    $.post("/handlers/login", JSON.stringify(json),
+        function (original) {
+            var data = JSON.parse(original)
+            var message
+            if (data.status == "succ") {
+                $.cookie("name", data.name)
+                $.cookie("uid", data.uID)
+                $.cookie("sessionID", data.sessionID)
+                $(window).attr('location', '/dashboard');
+            } else if (data == "fail") {
+                message = `登录失败，请重试！\n如果能登录官网，请<a href="/report">点此反馈</a>`
+                $("#msg").html(message)
+                $('#message').modal()
+            }
+        });
+}
+
+function create() {
+    var json = {
+        "uid": $.cookie("uid"),
+        "item": $("input[name='item']:checked").val(),
+        "amount": $("#amount").val(),
+        "kind": $("input[name='kind']:checked").val()
+    };
+    $.post("/handlers/create", JSON.stringify(json),
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else if (data == "Unauthorized") {
+                message = "请先登录"
+                $(window).attr('location', '/login');
+                return
+            } else if (data == "Success") {
+                message = "成功创建"
+            }
+            $("#msg").html(message)
+            $('#message').modal()
+        });
+}
+
+function match(orderID) {
+    if ($.cookie("uid") == null) {
+        $("#msg").text("请先登录！")
+        $('#message').modal()
+    } else {
+        $.post("/handlers/match", orderID,
+            function (data) {
+                var message
+                if (data == "Server Failure") {
+                    message = `服务器端异常，请<a href="/report">点此反馈</a>`
+                } else if (data == "Unauthorized") {
+                    $(window).attr('location', '/login');
+                    return
+                } else if (data == "Success") {
+                    message = "成功匹配"
+                }
+                $("#msg").text(message)
+                $('#message').modal()
+            });
+    }
+}
+
+function showInfo(role, orderID) {
+    var json = {
+        "uid": $.cookie("uid"),
+        "role": role,
+        "orderID": orderID
+    };
+    $.post("/handlers/oppositeInfo", JSON.stringify(json),
+        function (original) {
+            var data = JSON.parse(original)
+            if (data.status == "Unauthorized") {
+                $(window).attr('location', '/login');
+                return
+            } else if (data.status == "Success") {
+                $("#info").html(`姓名：` + data.name + `&nbsp;&nbsp;&nbsp;班级：` + data.class)
+            } else if (data.status == "Server Failure") {
+                $("#info").html(`服务器端异常，请<a href="/report">点此反馈</a>`)
+            }
+            $('#oppositeInfo').modal()
+        });
+}
+
+function del(orderID) {
+    $.post("/handlers/delete", orderID,
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else if (data == "Unauthorized") {
+                $(window).attr('location', '/login');
+                return
+            } else if (data == "Success") {
+                message = "成功删除"
+            }
+            $("#msg").text(message)
+            $('#message').modal()
+        });
+}
+
+function reject(orderID) {
+    $.post("/handlers/reject", orderID,
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else if (data == "Unauthorized") {
+                $(window).attr('location', '/login');
+                return
+            } else if (data == "Success") {
+                message = "成功拒绝"
+            }
+            $("#msg").text(message)
+            $('#message').modal()
+        });
+}
+
+function cancel(orderID) {
+    $.post("/handlers/cancel", orderID,
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else if (data == "Unauthorized") {
+                $(window).attr('location', '/login');
+                return
+            } else if (data == "Success") {
+                message = "成功取消"
+            }
+            $("#msg").text(message)
+            $('#message').modal()
+        });
+}
+
+function confirm(orderID) {
+    $.post("/handlers/confirm", orderID,
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else if (data == "Unauthorized") {
+                $(window).attr('location', '/login');
+                return
+            } else if (data == "Success") {
+                message = "成功确认"
+            }
+            $("#msg").text(message)
+            $('#message').modal()
+        });
+}
+
+function exit() {
+    $.post("/handlers/exit", $.cookie("uid"),
+        function (data) {
+            var message
+            if (data == "Server Failure") {
+                message = `服务器端异常，请<a href="/report">点此反馈</a>`
+            } else {
+                $.cookie("name", '', { expires: -1 })
+                $.cookie("uid", '', { expires: -1 })
+                $.cookie("sessionID", '', { expires: -1 })
+                $(window).attr('location', '/');
+            }
+            $("#msg").text(message)
+            $('#message').modal()
+        });
+}
+
+function launchFilterModal(index) {
+    $("#filterConfirm").click(function () {
+        var amount = $("#amount").val();
+        var type = $("input[name='type']:checked").val();
+        var grade = $("#grade").val();
+
+        var query = "#" + index + " tbody tr"
+
+        $(query).each(function () {
+            var a = $(this).children();
+            var amo = a[1].innerText;
+            var typ = a[2].innerText;
+            var gra = a[4].innerText;
+            if ((amount != "" && amo != amount) || (grade != "" && gra != grade) || typ != type) {
+                a.hide();
+            } else {
+                a.show();
+            }
+        })
+    })
+    $("#filter").modal()
+}
---
+++ src/tool/dbControl.go
@@ -0,0 +1,386 @@
+package tool
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	//匿名导入数据库驱动
+	_ "github.com/Go-SQL-Driver/MySQL"
+)
+
+var db *sql.DB
+var marketPlace string
+var isMarketPlaceModified bool
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+//CreateConnection 启动数据库连接
+func CreateConnection() bool {
+	newDB, err := sql.Open("mysql", "app:app@tcp(192.168.1.101:3306)/q5xy?charset=utf8")
+	checkErr(err)
+	db = newDB
+	isMarketPlaceModified = true
+	PraseMarketPlace()
+	return true
+}
+
+//NewUser 新建并记录用户信息（主要是班级）
+func NewUser(id string, name string, class string) bool {
+	_, err := db.Exec("insert into users (id,name,class) values (?,?,?)", id, name, class)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//HaveUser 检查数据库中是否有该用户
+func HaveUser(id string) bool {
+	rows, err := db.Query("select count(*) from users where id=?", id)
+	if err != nil {
+		return false
+	}
+	var count int
+	for rows.Next() {
+		err = rows.Scan(&count)
+		if err != nil {
+			return false
+		}
+	}
+	return (count == 1)
+}
+
+//HavePermission 检查用户是否拥有某订单的权限
+func HavePermission(uID string, role string, orderID string) bool {
+	rows, err := db.Query("select count(*) from orders where id=?", orderID)
+	if err != nil {
+		return false
+	}
+	var oCount int
+	for rows.Next() {
+		err = rows.Scan(&oCount)
+		if err != nil {
+			return false
+		}
+	}
+	result := true
+	if oCount != 1 {
+		result = false
+	} else {
+		if role == "0" {
+			rows, err = db.Query("select creator from orders where id=?", orderID)
+			if err != nil {
+				return false
+			}
+			var cID string
+			for rows.Next() {
+				err = rows.Scan(&cID)
+				if err != nil {
+					return false
+				}
+			}
+			if cID != uID {
+				result = false
+			}
+		} else if role == "1" {
+			rows, err = db.Query("select matcher from orders where id=?", orderID)
+			if err != nil {
+				return false
+			}
+			var mID string
+			for rows.Next() {
+				err = rows.Scan(&mID)
+				if err != nil {
+					return false
+				}
+			}
+			if mID != uID {
+				result = false
+			}
+		}
+	}
+	return result
+}
+
+//Create 创建新订单
+func Create(id string, item string, amount string, kind string) bool {
+	_, err := db.Exec("insert into orders (creator,item,amount,kind,date,status) values (?,?,?,?,now(),0)", id, item, amount, kind)
+	if err != nil {
+		return false
+	}
+	isMarketPlaceModified = true
+	return true
+}
+
+//Match 配对订单
+func Match(uID string, orderID string) bool {
+	rows, err := db.Query("select status from orders where id=?", orderID)
+	if err != nil {
+		return false
+	}
+	var status string
+	for rows.Next() {
+		err = rows.Scan(&status)
+		if err != nil {
+			return false
+		}
+	}
+	if status == "0" {
+		_, err = db.Exec("update orders set matcher=?,status=1 where id=?", uID, orderID)
+		if err != nil {
+			return false
+		}
+	}
+	isMarketPlaceModified = true
+	return true
+}
+
+//GetInfo 获取另一个用户的班级信息
+func GetInfo(uID string, role string, orderID string) (bool, string, string) {
+	rows, err := db.Query("select creator,matcher,status from orders where id=?", orderID)
+	if err != nil {
+		return false, "", ""
+	}
+	var creator, matcher, status string
+	name, class := "", ""
+	for rows.Next() {
+		err = rows.Scan(&creator, &matcher, &status)
+		if err != nil {
+			return false, "", ""
+		}
+	}
+	if status == "1" || status == "2" || status == "3" {
+		stmt, err := db.Prepare("select name,class from users where id=?")
+		if err != nil {
+			return false, "", ""
+		}
+
+		if role == "0" {
+			rows, err := stmt.Query(matcher)
+			if err != nil {
+				return false, "", ""
+			}
+			for rows.Next() {
+				err = rows.Scan(&name, &class)
+				if err != nil {
+					return false, "", ""
+				}
+			}
+		} else if role == "1" {
+			rows, err := stmt.Query(creator)
+			if err != nil {
+				return false, "", ""
+			}
+			for rows.Next() {
+				err = rows.Scan(&name, &class)
+				if err != nil {
+					return false, "", ""
+				}
+			}
+		}
+	}
+	return true, name, class
+}
+
+//Delete 删除订单
+func Delete(orderID string) bool {
+	_, err := db.Exec("delete from orders where id=?", orderID)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//Reject 拒绝配对请求
+func Reject(orderID string) (bool, string, string, string, string) {
+	rows, err := db.Query("select creator,item,amount,kind,status from orders where id=?", orderID)
+	if err != nil {
+		return false, "", "", "", ""
+	}
+	var creator, item, amount, kind, status string
+	for rows.Next() {
+		err = rows.Scan(&creator, &item, &amount, &kind, &status)
+		if err != nil {
+			return false, "", "", "", ""
+		}
+	}
+	if status == "1" {
+		_, err = db.Exec("update orders set status=\"3\" where id=?", orderID)
+		if err != nil {
+			return false, "", "", "", ""
+		}
+	}
+	isMarketPlaceModified = true
+	return true, creator, item, amount, kind
+}
+
+//Cancel 配对后取消配对请求
+func Cancel(orderID string) (bool, string, string, string, string) {
+	rows, err := db.Query("select creator,item,amount,kind,status from orders where id=?", orderID)
+	if err != nil {
+		return false, "", "", "", ""
+	}
+	var creator, item, amount, kind, status string
+	for rows.Next() {
+		err = rows.Scan(&creator, &item, &amount, &kind, &status)
+		if err != nil {
+			return false, "", "", "", ""
+		}
+	}
+	if status == "1" {
+		_, err = db.Exec("update orders set status=\"3\" where id=?", orderID)
+		if err != nil {
+			return false, "", "", "", ""
+		}
+	}
+	isMarketPlaceModified = true
+	return true, creator, item, amount, kind
+}
+
+//Confirm 确认已收到相应物品
+func Confirm(orderID string) bool {
+	rows, err := db.Query("select status from orders where id=?", orderID)
+	if err != nil {
+		return false
+	}
+	var status string
+	for rows.Next() {
+		err = rows.Scan(&status)
+		if err != nil {
+			return false
+		}
+	}
+	if status == "1" {
+		_, err = db.Exec("update orders set status=\"2\" where id=?", orderID)
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+//PraseTable 返回"/dashboard"页面的表格
+func PraseTable(id string) (bool, string) {
+	rows, err := db.Query("select count(*) from orders where creator=?", id)
+	if err != nil {
+		return false, ""
+	}
+	var cCount int
+	for rows.Next() {
+		err = rows.Scan(&cCount)
+		if err != nil {
+			return false, ""
+		}
+	}
+	rows, err = db.Query("select count(*) from orders where matcher=?", id)
+	if err != nil {
+		return false, ""
+	}
+	var mCount int
+	for rows.Next() {
+		err = rows.Scan(&mCount)
+		if err != nil {
+			return false, ""
+		}
+	}
+
+	result := ""
+	if cCount == 0 && mCount == 0 {
+		result = "Empty Set"
+	} else {
+		if cCount != 0 {
+			rows, err := db.Query("select id,item,amount,kind,date,status from orders where creator=?", id)
+			if err != nil {
+				return false, ""
+			}
+			for rows.Next() {
+				var (
+					id, item, amount, kind, date, status string
+				)
+				err := rows.Scan(&id, &item, &amount, &kind, &date, &status)
+				if err != nil {
+					return false, ""
+				}
+				result += id + "|" + item + "|" + amount + "|" + kind + "|" + date + "|" + status + "|0/"
+			}
+		}
+		result += "||"
+		if mCount != 0 {
+			rows, err := db.Query("select id,item,amount,kind,date,status from orders where matcher=?", id)
+			if err != nil {
+				return false, ""
+			}
+			for rows.Next() {
+				var (
+					id, item, amount, kind, date, status string
+				)
+				err := rows.Scan(&id, &item, &amount, &kind, &date, &status)
+				if err != nil {
+					return false, ""
+				}
+				result += id + "|" + item + "|" + amount + "|" + kind + "|" + date + "|" + status + "|1/"
+			}
+		}
+	}
+	return true, result
+}
+
+//PraseMarketPlace 返回/marketPlace页面的表格
+func PraseMarketPlace() (bool, string) {
+	if isMarketPlaceModified {
+		stmt, err := db.Prepare("select id,creator,amount,kind,date from orders where item=? and status=0")
+		if err != nil {
+			return false, ""
+		}
+		marketPlace = ""
+		for i := 0; i < 5; i++ {
+			rows, err := stmt.Query(i)
+			if err != nil {
+				return false, ""
+			}
+			/* if !rows.Next() {
+				marketPlace += "-||"
+				continue
+			} */
+			flag := false
+			for rows.Next() {
+				flag = true
+				var id, creator, amount, kind, date, class string
+				err = rows.Scan(&id, &creator, &amount, &kind, &date)
+				if err != nil {
+					return false, ""
+				}
+				user, err := db.Query("select class from users where id=?", creator)
+				if err != nil {
+					return false, ""
+				}
+				for user.Next() {
+					user.Scan(&class)
+				}
+				marketPlace += id + "|" + amount + "|" + kind + "|" + date + "|" + class + "/"
+			}
+			if !flag {
+				marketPlace += "-"
+			}
+			marketPlace += "||"
+		}
+	}
+	return true, marketPlace
+}
+
+//Log 自定义的日志函数
+func Log(action string, user string, ip string, status string) {
+	_, err := db.Exec("insert into logs (time,action,user,ip,status) values (now(),?,?,?,?)", action, user, ip, status)
+	if status == "fatal" {
+		fmt.Printf(`Fatal error when user "%s" from IP "%s" is performing action "%s"`, user, ip, action)
+		os.Exit(1)
+	} else if err != nil {
+		fmt.Println(`Fail to save logs`)
+		os.Exit(1)
+	}
+}
